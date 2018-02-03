b4cd124995665432fe06b5f29848171ca62bf44b
Julius Suominen <julius.suominen@gmail.com>
2018-02-03 18:27:05 +0100

    Handle ADD_MESSAGE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-function createStore(reducer) {
-  let state = 0;
+function createStore(reducer, initialState) {
+  let state = initialState;
 
   const getState = () => (state);
 
@@ -12,3 +12,37 @@ function createStore(reducer) {
     dispatch,
   };
 }
+
+function reducer(state, action) {
+  if (action.type === 'ADD_MESSAGE') {
+    return {
+      // concat because reducers have to be pure functions.
+      // concat creates a new copy of the given array
+      messages: state.messages.concat(action.message)
+    };
+  } else {
+    return state;
+  }
+}
+
+const initialState = { messages: [] };
+const store = createStore(reducer, initialState);
+
+const addMessageAction1 = {
+  type: 'ADD_MESSAGE',
+  message: 'How does it look, Neil?',
+  };
+
+  store.dispatch(addMessageAction1);
+  
+  const stateV1 = store.getState();
+    const addMessageAction2 = { type: 'ADD_MESSAGE', message: 'Looking good.',
+  };
+
+  store.dispatch(addMessageAction2);
+  const stateV2 = store.getState();
+  
+  console.log('State v1:');
+  console.log(stateV1);
+  console.log('State v2:');
+  console.log(stateV2);
